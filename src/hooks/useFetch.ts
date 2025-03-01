0239493be318d618b87593fd167f66a8b81a1a18
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-01 13:19:58 +0300

    feat(useFetch): Добавлен AbortController

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -5,10 +5,10 @@ interface IFetchBody {
 }
 
 interface IUseFetch<T> {
-  data: T | null
-  isLoading: boolean
-  error: string | null
-  refetch: (params?: IFetchBody) => void
+	data: T | null
+	isLoading: boolean
+	error: string | null
+	refetch: (params?: IFetchBody) => void
 }
 
 export default function useFetch<T>(url: string, body?: IFetchBody): IUseFetch<T> {
@@ -17,11 +17,10 @@ export default function useFetch<T>(url: string, body?: IFetchBody): IUseFetch<T
 	const [isLoading, setIsLoading] = useState(false)
 	const [error, setError] = useState<string | null>(null)
 
-
-
-	const send = async (fetchUrl: string, body: IFetchBody = { params: {} }) => {
+	const send = async (fetchUrl: string, body: IFetchBody = { params: {} }, signal?: AbortSignal) => {
 		try {
 			setIsLoading(true)
+
 
 			const url: URL = new URL(fetchUrl)
 
@@ -31,17 +30,23 @@ export default function useFetch<T>(url: string, body?: IFetchBody): IUseFetch<T
 			url.search = searchParams.toString()
 
 
-			const res = await fetch(url).then(res => res.json())
+			const res = await fetch(url, {
+				signal,
+			})
 
-			if(res.ok)
-				setData(res)
+			if (res.ok) {
+				const data = await res.json()
+				setData(data)
+			}
 
 			setIsLoading(false)
 
 		} catch (e) {
+			if(signal?.aborted) return
+
 			setData(null)
-			
-			if(e instanceof Error)
+
+			if (e instanceof Error)
 				setError(e.message)
 
 			console.warn(e)
@@ -56,7 +61,12 @@ export default function useFetch<T>(url: string, body?: IFetchBody): IUseFetch<T
 	}
 
 	useEffect(() => {
-		send(url, body)
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+		send(url, body, signal)
+
+		return () => controller.abort()
 	}, [url, body])
 
 
