830d2669c120af642783848a0dbb145ab3b63560
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-01 13:53:53 +0300

    feat(useLocalStorage): Поправлены функции getValueStorage, setItem

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,9 +1,6 @@
 import { useState } from "react";
 function getValueStorage(key: string): LocalStorageReturnValue {
-	const saveValue: string = JSON.parse(localStorage.getItem(key) || 'null')
-
-	if (!saveValue)
-		return null
+	const saveValue: string | null = localStorage.getItem(key)
 
 	return saveValue
 }
@@ -13,8 +10,8 @@ const useLocalStorage: UseLocalStorage = (key) => {
 	const [value, setValue] = useState<LocalStorageReturnValue>(() => getValueStorage(key))
 
 	const setItem = (value: LocalStorageSetValue) => {
-		localStorage.setItem(key, JSON.stringify(value))
-		setValue(getValueStorage(key))
+		localStorage.setItem(key, value)
+		setValue(value)
 	}
 
 	const removeItem = (): void => {
